81c25aa7f7fb601d8e4b7fec7fabcc64f7c51d9b
jujujuz <jujujuz@b65c1c68-2513-11de-8c12-6dbec8df7899>
2011-05-05 12:44:23 +0000

    maj petankparty ok 2011

--- trunk/groovy/org/petank/server/rest/ChartResource.groovy
+++ trunk/groovy/org/petank/server/rest/ChartResource.groovy
@@ -39,7 +39,7 @@ public class ChartResource extends PlayerResource {
 		//def pointmin = points.min()
 		//def pointmax = points.max()
 		//String a = "http://chart.apis.google.com/chart?chs=500x200&cht=lc&chds=550,800&chxt=y&chxl=0:|550|600|650|700|750|800&chxp=550,600,650,700,750,800|&chtt=${player.name}&chd=t:${allpoints}"
-		String gString = """<img src="http://chart.apis.google.com/chart?chs=500x200&amp;cht=lc&amp;chds=0,400&amp;chxt=y&amp;chxl=0:|50|100|150|200|250|300|350|400&amp;chxp=0,50,100,150,200,250,300,350,400|&amp;chtt=${player.name}&amp;chd=t:${allpoints}" alt="Chart ${player.name}" />"""		
+		String gString = """<img src="http://chart.apis.google.com/chart?chs=500x200&amp;cht=lc&amp;chds=0,250&amp;chxt=y&amp;chxl=0:|0|50|100|150|200|250&amp;chxp=0,50,100,150,200,250|&amp;chtt=${player.name}&amp;chd=t:100.0,${allpoints}" alt="Chart ${player.name}" />"""		
 	}
 
 }
